1d9c20985e2c22b23f72dac72ce650bff9a3337b
Daniel Higuita <daniel.higuita007@gmail.com>
2019-03-12 11:11:14 -0500

    second

---
+++ weapons/weapon_information_enemies.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var type_of_bullets = {"weapon_type_one": preload("res://projectiles/blue_projectile/BlueProjectile.tscn")}
+var weapons_carriage = {"weapon_type_one": 10}
+var current_bullets_in_carriage = {"weapon_type_one": 10}
+var shoot_cool_down_timers = {"weapon_type_one": 0.5}
+var reload_cool_down_timers = {"weapon_type_one": 1}
