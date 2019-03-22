b7202f58f6f47388a78c0f727ae845b00d7f3b78
Daniel Higuita <daniel.higuita007@gmail.com>
2019-03-22 00:14:07 -0500

    commit

--- world/mob_handler/enemies_instructions/enemies_instructions.gd
+++ world/mob_handler/enemies_instructions/enemies_instructions.gd
@@ -1,9 +1,13 @@
 extends Node
 
+var enemy_names = ["small_ship", "normal_ship", "gray_asteroid", "brow_asteroid",
+"dark_asteroid"]
+
 var enemies_quantity = {
 	"small_ship": 10,
 	"normal_ship": 8,
 	"gray_asteroid": 20,
 	"brow_asteroid": 20,
 	"dark_asteroid": 15} 
+
 var enemies_blue_print = preload("res://world/mob_handler/blue_print_enemies/blue_print_enemies.gd").new()
