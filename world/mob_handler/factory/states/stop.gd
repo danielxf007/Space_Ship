40b6d5d64828e48cc5f3af3b1884f4c0b081b414
Daniel Higuita <daniel.higuita007@gmail.com>
2019-03-21 22:52:54 -0500

    commit

--- world/mob_handler/factory/states/stop.gd
+++ world/mob_handler/factory/states/stop.gd
@@ -2,7 +2,9 @@ extends "res://world/mob_handler/factory/states/state.gd"
 
 var blue_prints
 func enter():
-	return
+	create_finished_goods_queue()
+	create_manufacture_queue()
+
 
 func exit():
 	return
@@ -17,9 +19,13 @@ func add_to_manufacture_queue(good_name):
 	var model_info = consult_blue_prints(good_name)
 	owner.manufacture_queue.push_back(model_info)
 
-func create_manufacture_queue(queue):
+func create_manufacture_queue():
 	var new_queue = []
 	owner.set_manufacture_queue(new_queue)
 
+func create_finished_goods_queue():
+	var new_queue = []
+	owner.finished_goods(new_queue)
+	
 func consult_blue_prints(good_name):
 	return blue_prints[good_name]
